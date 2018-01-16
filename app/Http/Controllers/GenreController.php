e8435ac9fdd6c8c4f64f5a562d19ddad0363689c
jordanf97 <jordanfischer97@gmail.com>
2018-01-17 01:20:12 +1000

    Initial setup of API endpoints completed
    The basis of the required endpoints have been completed - conveniantly totalling 10

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Genre;
 
 class GenreController extends Controller
 {
-    //
+    public function index($movieID) {
+        return Genre::select('*')->where('movieID', $movieID)->get();
+    }
+
+    public function store(Request $request) {
+        return Actor::create([
+            'genre' => $request->input('genre'),
+            'movieID'   => $request->input('movieID')
+        ]);
+    }
 }
