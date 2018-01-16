ffd1974577d5ddb34d522836b8e1f717176cf54e
jordanf97 <jordanfischer97@gmail.com>
2018-01-17 00:56:17 +1000

    Being movie endpoint
    Begin work on the movies endpoint

---
+++ database/migrations/2018_01_16_145337_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 60);
+            $table->string('synopsis', 200);
+            $table->string('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
